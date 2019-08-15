ae3ac69f36dd1e92974df15af0c9ae06cd87836f
Antoine Benevaut <antoine@benevaut.fr>
2019-08-15 14:20:35 +0200

    feat: add deploy_rsa key to travis to allow to access private repositories and deploy to fortrabbit

---
+++ scripts/prepare_travis_enc_files.sh
@@ -0,0 +1,10 @@
+if [ -e deploy_rsa ]
+then
+    echo "[OK] deploy_rsa"
+else
+    echo "[KO] deploy_rsa"
+    exit
+fi
+
+tar cvf travis.tar deploy_rsa
+travis encrypt-file travis.tar -r obsession-city/www
