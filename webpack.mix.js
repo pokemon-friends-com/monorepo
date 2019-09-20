de9e0ea3c3debd675b8d3784bd86bf639ef34d0b
Antoine B <antoine@benevaut.fr>
2019-09-20 23:19:44 +0200

    chore: prepare project to be watched by scsslint (#12)
    * chore: prepare project to be watched by scsslint
    * scss lint when running webpack
    * precise exclude patterns in codeclimate config file
    * fix nyc coverage

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,54 +1,32 @@
 const mix = require('laravel-mix');
+const path = require('path');
+const imageminMozjpeg = require('imagemin-mozjpeg');
 const ImageminPlugin = require('imagemin-webpack-plugin').default;
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-const imageminMozjpeg = require('imagemin-mozjpeg');
-const path = require('path');
+const StyleLintPlugin = require('stylelint-webpack-plugin');
 
-mix.webpackConfig({
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './resources/js'),
-    },
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre',
-        test: /\.(js|vue)$/,
-        loader: 'eslint-loader',
-        exclude: /(node_modules|tests)/,
+mix
+  .webpackConfig({
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, './resources/js'),
       },
-    ],
-  },
-});
-
-/*
- |--------------------------------------------------------------------------
- | Mix Asset Management
- |--------------------------------------------------------------------------
- |
- | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel application. By default, we are compiling the Sass
- | file for the application as well as bundling up all the JS files.
- |
- */
-
-mix.js('resources/js/app.js', 'public/js')
-  .js('resources/js/backend.js', 'public/js')
-  .sass('resources/sass/app.scss', 'public/css')
-  .sass('resources/sass/backend.scss', 'public/css')
-  .copy('resources/gameforest/dist/css', 'public/css')
-  .copy('resources/gameforest/dist/js', 'public/js')
-  .copy('resources/gameforest/dist/fonts', 'public/fonts')
-  .copy('resources/pages/dist/pages/css', 'public/css')
-  .copy('resources/pages/dist/pages/js', 'public/js')
-  .copy('resources/pages/dist/pages/fonts', 'public/fonts')
-  .copy('resources/pages/dist/pages/ico', 'public/ico')
-  .copy('resources/pages/plugins', 'public/plugins');
-
-if (mix.config.production) {
-  mix.webpackConfig({
+    },
+    module: {
+      rules: [
+        {
+          enforce: 'pre',
+          test: /\.(js|vue)$/,
+          loader: 'eslint-loader',
+          exclude: /(node_modules|tests)/,
+        },
+      ],
+    },
     plugins: [
+      new StyleLintPlugin({
+        configFile: '.stylelintrc',
+        context: 'resources/sass',
+      }),
       new CopyWebpackPlugin([
         {
           from: 'resources/img',
@@ -74,5 +52,35 @@ if (mix.config.production) {
         ],
       }),
     ],
-  }).sourceMaps().version();
+  });
+
+/*
+ |--------------------------------------------------------------------------
+ | Mix Asset Management
+ |--------------------------------------------------------------------------
+ |
+ | Mix provides a clean, fluent API for defining some Webpack build steps
+ | for your Laravel application. By default, we are compiling the Sass
+ | file for the application as well as bundling up all the JS files.
+ |
+ */
+
+mix
+  .js('resources/js/app.js', 'public/js')
+  .js('resources/js/backend.js', 'public/js')
+  .sass('resources/sass/app.scss', 'public/css')
+  .sass('resources/sass/backend.scss', 'public/css')
+  .copy('resources/gameforest/dist/css', 'public/css')
+  .copy('resources/gameforest/dist/js', 'public/js')
+  .copy('resources/gameforest/dist/fonts', 'public/fonts')
+  .copy('resources/pages/dist/pages/css', 'public/css')
+  .copy('resources/pages/dist/pages/js', 'public/js')
+  .copy('resources/pages/dist/pages/fonts', 'public/fonts')
+  .copy('resources/pages/dist/pages/ico', 'public/ico')
+  .copy('resources/pages/plugins', 'public/plugins');
+
+if (mix.config.production) {
+  mix.version();
+} else {
+  mix.sourceMaps();
 }
