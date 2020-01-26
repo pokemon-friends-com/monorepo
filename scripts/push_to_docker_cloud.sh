6951fe89519b075b40887a6da1a11dca3d3e2ff1
Antoine B <antoine@benevaut.fr>
2020-01-26 23:04:14 +0100

    realease: 0.1.1 (#67)
    * prepare new fake release
    * rename staging branch to develop
    * remove docker deploy script
    * add fortrabbit deploy script
    * update faveicon
    * update travis rules

--- scripts/push_to_docker_cloud.sh
+++
@@ -1,2 +0,0 @@
-docker login --username $DOCKER_USERNAME --password $DOCKER_PASSWORD
-docker-compose --project-name template push workspace apache2 mailhog php-fpm php-worker mysql redis elasticsearch kibana
