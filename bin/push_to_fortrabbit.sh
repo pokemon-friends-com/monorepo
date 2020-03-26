692c3a890a1be8f5c5ee8c73af5e9dca35dd787e
Antoine B <antoine@benevaut.fr>
2020-03-26 18:06:20 +0100

    feat(travis-ci): update to pro host on fortrabbit.com (#81)

--- bin/push_to_fortrabbit.sh
+++ bin/push_to_fortrabbit.sh
@@ -1,15 +1,15 @@
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan down
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan backup:run --only-db --filename $TRAVIS_JOB_NUMBER.zip
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan config:clear
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan route:clear
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan view:clear
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan cache:clear
-git remote add fortrabbit pokemon-friends@deploy.eu2.frbit.com:pokemon-friends.git
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan down
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan backup:run --only-db --filename $TRAVIS_JOB_NUMBER.zip
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan config:clear
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan route:clear
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan view:clear
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan cache:clear
+git remote add fortrabbit pkmn-friends@deploy.eu2.frbit.com:pkmn-friends.git
 git push fortrabbit master
-scp -r ./public pokemon-friends@deploy.eu2.frbit.com:/srv/app/pokemon-friends/htdocs
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan config:cache
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan route:cache
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan view:clear
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan cache:clear
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan migrate --force
-ssh pokemon-friends@deploy.eu2.frbit.com php artisan up
+scp -r ./public pkmn-friends@deploy.eu2.frbit.com:/srv/app/pkmn-friends/htdocs
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan config:cache
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan route:cache
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan view:clear
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan cache:clear
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan migrate --force
+ssh pkmn-friends@deploy.eu2.frbit.com php artisan up
