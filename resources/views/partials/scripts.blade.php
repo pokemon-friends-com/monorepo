aba1809815cd1cdbbdb7087424a51ee035a6a62e
Antoine Benevaut <antoine@benevaut.fr>
2020-03-27 11:16:18 +0100

    feat(app): configure fortrabbit object-storage

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -1,3 +1,3 @@
 <script src="https://www.nayuki.io/res/qr-code-generator-library/qrcodegen.js"></script>
-<script src="{{ mix('js/app.js') }}"></script>
+<script src="{{ mix_cdn('js/app.js') }}"></script>
 @yield('js')
