bc2a2be9615945f0e49aa0de0670290c929d1724
Antoine Benevaut <antoine@benevaut.fr>
2020-03-27 13:31:46 +0100

    feat(app): remove fortrabbit object-storage

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'default' => 's3',
+    'default' => 'local',
 
     /*
     |--------------------------------------------------------------------------
@@ -43,17 +43,17 @@ return [
 
     'disks' => [
 
-//        'local' => [
-//            'driver' => 'local',
-//            'root' => storage_path('app'),
-//        ],
+        'local' => [
+            'driver' => 'local',
+            'root' => storage_path('app'),
+        ],
 
-//        'public' => [
-//            'driver' => 'local',
-//            'root' => storage_path('app/public'),
-//            'url' => env('APP_URL') . '/storage',
-//            'visibility' => 'public',
-//        ],
+        'public' => [
+            'driver' => 'local',
+            'root' => storage_path('app/public'),
+            'url' => env('APP_URL') . '/storage',
+            'visibility' => 'public',
+        ],
 
         's3' => [
             'driver' => 's3',
