27541fd3abb0caf4beddf9ae8562d9f6c81f422f
Antoine B <antoine@benevaut.fr>
2020-04-18 19:43:52 +0200

    feat(app): some adjustments for trainers profiles (#135)

--- resources/views/anonymous/users/users/show.blade.php
+++ resources/views/anonymous/users/users/show.blade.php
@@ -9,7 +9,11 @@
     <div class="card-body login-card-body text-center">
         <h1 class="text-primary">{{ $friend_code }}</h1>
         <img src="{{ $qr }}" alt="{{ $friend_code }}">
+        @if (Auth::check())
+            <a href="{{ route('anonymous.dashboard') }}" class="btn btn-primary btn-block">{{ trans('users.home') }}</a>
+        @else
         <a href="{{ route('register') }}" class="btn btn-primary btn-block mt-4">{{ trans('auth.register') }}</a>
         <a href="{{ route('anonymous.dashboard') }}" class="btn btn-outline-secondary btn-block">{{ trans('users.home') }}</a>
+        @endif
     </div>
 @endsection
