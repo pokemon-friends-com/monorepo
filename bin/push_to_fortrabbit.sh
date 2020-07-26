75b15a2bf65143f4bd85a6d49eb3928595364fc4
Antoine B <antoine@benevaut.fr>
2020-07-26 19:17:38 +0200

    feat(ci): deploy with gh actions (#331)
    * use remote ssh command execution
    * add known hosts as gh secrets var
    * run only on release

--- bin/push_to_fortrabbit.sh
+++
@@ -1,8 +0,0 @@
-ssh pkmn-friends@deploy.eu2.frbit.com php artisan config:clear
-ssh pkmn-friends@deploy.eu2.frbit.com php artisan route:clear
-git remote add fortrabbit pkmn-friends@deploy.eu2.frbit.com:pkmn-friends.git
-git push fortrabbit master
-ssh pkmn-friends@deploy.eu2.frbit.com php artisan config:cache
-ssh pkmn-friends@deploy.eu2.frbit.com php artisan route:cache
-ssh pkmn-friends@deploy.eu2.frbit.com php artisan view:clear
-ssh pkmn-friends@deploy.eu2.frbit.com php artisan migrate --force
