766b3480e6c7816b631ac18b6f59aeec8c536589
Antoine B <antoine@benevaut.fr>
2020-08-08 08:39:55 +0200

    release: 1.2.5 (#370)
    * bump: 1.2.5 (#369)
    * remove adminlte
    * install sweetalert2
    * delete app js and scss
    * do not use holderjs from theme

--- resources/js/theme.js
+++ resources/js/theme.js
@@ -1,91 +1,42 @@
-import './bootstrap';
-import 'ekko-lightbox';
-import '@abenevaut/gameforest/src/js/index';
-import Vue from 'vue';
-import LazyLoad from 'vanilla-lazyload';
-import ClipboardJS from 'clipboard';
-import Swal from 'admin-lte/plugins/sweetalert2/sweetalert2.all';
-import * as Sentry from '@sentry/browser';
-import * as Integrations from '@sentry/integrations';
-import VueInternationalization from 'vue-i18n';
-import Locale from './vue-i18n-locales.generated';
+// copy of @abenevaut/gameforest/src/js/index
+import 'popper.js';
+import 'jquery';
+import 'bootstrap';
+import 'bootstrap-select';
+import 'flatpickr';
+// import 'holderjs' // do not pass tests
+import 'easy-pie-chart/dist/jquery.easypiechart';
+import 'jquery-parallax.js';
+import 'lazysizes';
+import 'nouislider';
+import 'datatables';
+import 'datatables.net-bs4';
+import Background from '@abenevaut/gameforest/src/js/background';
+import Carousel from '@abenevaut/gameforest/src/js/carousel';
+import Disqus from '@abenevaut/gameforest/src/js/disqus';
+import Embed from '@abenevaut/gameforest/src/js/embed';
+import Facebook from '@abenevaut/gameforest/src/js/facebook';
+import Lightbox from '@abenevaut/gameforest/src/js/lightbox';
+import Navbar from '@abenevaut/gameforest/src/js/navbar';
+import Notify from '@abenevaut/gameforest/src/js/notify';
+import Progress from '@abenevaut/gameforest/src/js/progress';
+import Sticky from '@abenevaut/gameforest/src/js/sticky';
+import Style from '@abenevaut/gameforest/src/js/style';
+import Svg from '@abenevaut/gameforest/src/js/svg';
+import Theme from '@abenevaut/gameforest/src/js/theme';
 
-Sentry.init({
-  dsn: process.env.MIX_SENTRY_PUBLIC_DSN,
-  debug: process.env.MIX_APP_DEBUG,
-  release: process.env.MIX_APP_TAG,
-  environment: process.env.MIX_APP_ENV,
-  integrations: [
-    new Integrations.Vue({
-      Vue,
-      attachProps: true,
-    }),
-  ],
-});
-
-/**
- * Vue i18n
- */
-
-Vue.use(VueInternationalization);
-const i18n = new VueInternationalization({
-  locale: document.head.querySelector('meta[name="locale"]'),
-  fallbackLocale: 'en',
-  messages: Locale,
-});
-
-/**
- * Vue filters
- */
-
-Vue.filter('pkmnFriendCode', (code) => `${code.slice(0, 4)}-${code.slice(4, 8)}-${code.slice(8, 12)}`);
-
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
-
-const files = require.context('./components/', true, /\.vue$/i);
-// eslint-disable-next-line
-files.keys().map((key) => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-
-// eslint-disable-next-line
-const app = new Vue({
-  el: '#app',
-  i18n,
-});
-
-(new LazyLoad({
-  elements_selector: '.lazy',
-}))
-  .update();
-(new ClipboardJS('.btn-copy'))
-  .on(
-    'success',
-    (event) => {
-      Swal
-        .mixin({
-          toast: true,
-          position: 'bottom-end',
-          showConfirmButton: false,
-          timer: 3000,
-        })
-        .fire({
-          type: 'success',
-          title: app.$t('global.copied'),
-        });
-      event.clearSelection();
-    },
-  );
-$(() => {
-  $('.easypiechart').easyPieChart();
-});
+export {
+  Background,
+  Carousel,
+  Disqus,
+  Embed,
+  Facebook,
+  Lightbox,
+  Navbar,
+  Notify,
+  Progress,
+  Sticky,
+  Style,
+  Svg,
+  Theme,
+};
